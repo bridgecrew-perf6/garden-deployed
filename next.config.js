fcb99726373459ce43b66818747e2ee8f7a098c2
rpo <hakimnaufal212@gmail.com>
2022-04-08 23:52:35 +0700

    feat: configuring nextjs github-page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/garden-deployed/' : ''
+}
