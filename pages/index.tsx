999f57b9945c0320817a233cbac03160cc69a16d
rpo <hakimnaufal212@gmail.com>
2022-04-09 14:51:04 +0700

    chore: deleting unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import MainDashboard from '../components'
-import styles from '../styles/Home.module.css'
+import React from "react";
+
+
 
 const Home: NextPage = () => {
   return (
-    <MainDashboard />
+      <MainDashboard />
   )
 }
 
